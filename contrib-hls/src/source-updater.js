b0ba40f27d352a1bd4cece1d346881d7d6358088
Mayde <maysjtu@163.com>
2018-06-14 11:31:04 +0800

    contrib-hls save

---
+++ contrib-hls/src/source-updater.js
@@ -0,0 +1,19 @@
+/**
+ * @file source-updater.js
+ */
+import videojs from 'video.js';
+
+const noop = function() {};
+/**
+ * A queue of callbacks to be serialized and applied when a
+ * MediaSource and its associated SourceBuffers are not in the
+ * updating state. It is used by the segment loader to update the
+ * underlying SourceBuffers when new data is loaded, for instance.
+ *
+ * @class SourceUpdater
+ * @param {MediaSource} mediaSource the MediaSource to create the
+ * SourceBuffer from
+ * @param {String} mimeType the desired MIME type of the underlying
+ * SourceBuffer
+ */
+
